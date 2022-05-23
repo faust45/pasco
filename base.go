5743b215c0f0748de105d9484fef484adea7701b
Your Name <partosqq@gmail.com>
2022-05-23 18:12:58 +0300

    first commit

---
+++ base.go
@@ -0,0 +1,200 @@
+package db
+
+import (
+	// "menubot/coll"
+	// "errors"
+
+	// "strings"
+	// "bytes"
+	"encoding/binary"
+	// "encoding/json"
+	// "strings"
+	// "gopkg.in/yaml.v2"
+	"bytes"
+	bolt "go.etcd.io/bbolt"
+	"log"
+	time "time"
+	// "io/ioutil"
+	// "net/http"
+	// "math/rand"
+)
+
+var (
+	db             *bolt.DB
+	indexes        map[string]Index
+	updatesIndexes map[string]Index
+)
+
+type ID uint64
+type Timestamp int64
+
+type Doc interface {
+	BucketName() string
+	ID() ID
+	MarshalJson() ([]byte, error)
+}
+
+type Conf struct {
+	File    string
+	Indexes []Index
+	Coll    []string
+}
+
+func Open(conf Conf) error {
+	var err error
+	db, err = bolt.Open(conf.File, 0600, nil)
+	if err != nil {
+		return err
+	}
+
+	updatesColl := mapCollections(conf.Coll)
+	indexesColl := indexSetRoot(conf.Indexes, "indexes")
+
+	err = initIndexes(append(indexesColl, updatesColl...))
+	if err != nil {
+		return err
+	}
+
+	updatesIndexes = mapByName(updatesColl)
+	indexes = mapByName(indexesColl)
+
+	return nil
+}
+
+func initIndexes(coll []Index) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		for _, idx := range coll {
+			if err := idx.init(tx); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+}
+
+func Close() {
+	db.Close()
+}
+
+func DB() *bolt.DB {
+	return db
+}
+
+func Save(doc Doc) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		bucketName := doc.BucketName()
+		b, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+		if err != nil {
+			return err
+		}
+
+		data, err := doc.MarshalJson()
+		if err != nil {
+			log.Printf("db.Save: Marshal %s", err)
+			return err
+		}
+
+		key := doc.ID().Bytes()
+		err = b.Put(key, data)
+		if err != nil {
+			log.Printf("db.Save: Put data %s\n %s %s", err, doc.ID(), data)
+			return err
+		}
+
+		return logUpdates(tx, doc)
+	})
+}
+
+func GenId() ID {
+	return ID(time.Now().UnixNano())
+}
+
+func Search(indexName, start, end string) {
+	index := indexes[indexName]
+	index.Update()
+
+	db.View(func(tx *bolt.Tx) error {
+		broot := tx.Bucket([]byte("indexes"))
+		b := broot.Bucket([]byte(index.Name))
+		c := b.Cursor()
+		k, v := c.Seek([]byte(start))
+
+		for k != nil {
+			log.Printf("in for %s, %+v", k, v)
+			k, v = c.Next()
+		}
+
+		return nil
+	})
+}
+
+func mapCollections(coll []string) []Index {
+	var indexes []Index
+	for _, name := range coll {
+		idx := Index{
+			root:       "updates",
+			BucketName: name,
+			Name:       name,
+		}
+
+		indexes = append(indexes, idx)
+	}
+
+	return indexes
+}
+
+func bytesToId(b []byte) ID {
+	return ID(binary.LittleEndian.Uint64(b))
+}
+
+func logUpdates(tx *bolt.Tx, doc Doc) error {
+	index := updatesIndexes[doc.BucketName()]
+	windex, _ := indexWriter(tx, index)
+
+	k, v := GenId().Bytes(), doc.ID().Bytes()
+	return windex(k, v)
+}
+
+func intToBytes(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+	}
+
+	return buff.Bytes()
+}
+
+func (id ID) Bytes() []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(id))
+
+	return b
+}
+
+func GetTimestamp() Timestamp {
+	return Timestamp(time.Now().UnixNano())
+}
+
+func (t Timestamp) Bytes() []byte {
+	return intToBytes(int64(t))
+}
+
+func mapByName(coll []Index) map[string]Index {
+	m := make(map[string]Index)
+	for _, idx := range coll {
+		m[idx.Name] = idx
+	}
+
+	return m
+}
+
+func indexSetRoot(coll []Index, root string) []Index {
+	var arr []Index
+	for _, idx := range coll {
+		idx.root = root
+		arr = append(arr, idx)
+	}
+
+	return arr
+}
---
+++ index.go
@@ -0,0 +1,156 @@
+package db
+
+import (
+	bolt "go.etcd.io/bbolt"
+	"log"
+	// time "time"
+	// "encoding/binary"
+	// "math/rand"
+)
+
+type IterFn func(HandlerIterFn) (bool, error)
+type HandlerIterFn func([]byte, []byte) error
+type IndexFn func([]byte) ([]byte, error)
+type IndexWriterFn func([]byte, []byte) error
+type IndexCleanerFn func([]byte) error
+
+type Index struct {
+	root       string
+	BucketName string
+	Name       string
+	Fun        IndexFn
+}
+
+func (index Index) Update() error {
+	log.Printf("updateInBatch start")
+
+	for done := false; !done; {
+		var err error
+		db.Update(func(tx *bolt.Tx) error {
+			done, err = index.batchUpdate(tx, 1000)
+			return err
+		})
+	}
+
+	log.Printf("updateInBatch success")
+	return nil
+}
+
+func indexWriter(tx *bolt.Tx, index Index) (IndexWriterFn, IndexCleanerFn) {
+	rbucketName := []byte("reverse/" + index.Name)
+	broot := tx.Bucket([]byte(index.root))
+	b := broot.Bucket([]byte(index.Name))
+	rb := broot.Bucket(rbucketName)
+
+	cleaner := func(docId []byte) error {
+		oldKey := rb.Get(docId)
+		err := b.Delete(oldKey)
+		if err != nil {
+			return err
+		}
+
+		return rb.Delete(docId)
+	}
+
+	writer := func(key, docId []byte) error {
+		key = append(key, docId...)
+
+		oldKey := rb.Get(docId)
+		err := b.Delete(oldKey)
+		if err != nil {
+			return err
+		}
+
+		err = b.Put(key, docId)
+		if err != nil {
+			return err
+		}
+
+		return rb.Put(docId, key)
+	}
+
+	return writer, cleaner
+}
+
+func (index Index) init(tx *bolt.Tx) error {
+	broot, err := tx.CreateBucketIfNotExists([]byte(index.root))
+	if err != nil {
+		return err
+	}
+
+	_, err = broot.CreateBucketIfNotExists([]byte(index.Name))
+	if err != nil {
+		return err
+	}
+
+	rbucketName := []byte("reverse/" + index.Name)
+	_, err = broot.CreateBucketIfNotExists(rbucketName)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (index Index) recentUpdatesIter(tx *bolt.Tx, batchSize int) IterFn {
+	b := tx.Bucket([]byte("updates"))
+	c := b.Bucket([]byte(index.BucketName)).Cursor()
+	bsource := tx.Bucket([]byte(index.BucketName))
+
+	klast := []byte("lastUpdate/" + index.BucketName)
+	lastUpdate := b.Get(klast)
+
+	var key, id []byte
+	if lastUpdate != nil {
+		c.Seek(lastUpdate)
+		key, id = c.Next()
+	} else {
+		key, id = c.First()
+	}
+
+	return func(fun HandlerIterFn) (bool, error) {
+		for i := 1; i != batchSize; i++ {
+			if key == nil {
+				return true, nil
+			}
+
+			data := bsource.Get(id)
+			err := fun(id, data)
+			if err != nil {
+				return false, err
+			}
+
+			err = b.Put(klast, key)
+			if err != nil {
+				return false, err
+			}
+
+			key, id = c.Next()
+		}
+
+		return false, nil
+	}
+}
+
+func (index Index) batchUpdate(tx *bolt.Tx, batchSize int) (bool, error) {
+	var key []byte
+	var err error
+
+	updateIndex, cleanIndex := indexWriter(tx, index)
+	iter := index.recentUpdatesIter(tx, batchSize)
+
+	return iter(func(id, data []byte) error {
+		if data != nil {
+			key, err = index.Fun(data)
+			if err != nil {
+				log.Printf("Err index Fun %s %s", err, data)
+				return err
+			}
+
+			return updateIndex(key, id)
+		}
+
+		//in case doc deleted
+		return cleanIndex(id)
+	})
+}
